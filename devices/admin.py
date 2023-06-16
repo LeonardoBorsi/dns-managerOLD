ed8981acb893d02a244cc79a6da08fd7eec32c87
Leonardo Borsi <l.borsi@hu-care.com>
2023-06-16 10:24:01 +0200

    devices

--- devices/admin.py
+++ devices/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Device
+from .models import (Device, Port)
 
 admin.site.register(Device)     # display Device modal in admin
+admin.site.register(Port)     # display Port modal in admin
--- devices/models.py
+++ devices/models.py
@@ -4,10 +4,19 @@ from django.urls import reverse
 class Device(models.Model):
     ip_address = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=50)
+    accepted = models.BooleanField(null=True)
 
-    def __str__(self) -> str:   # displayed row name in admin
-        return str(self.pk) + " " + str(self.ip_address) + " " + str(self.name)
+    def __str__(self):
+        return str(self.pk) + " | " + str(self.ip_address) + " - " + str(self.name)
     
     def get_absolute_url(self):
       return reverse("device_detail", kwargs={"pk": self.pk})
+    
+class Port(models.Model):
+    number = models.IntegerField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+    accepted = models.BooleanField(null=True)
+
+    def __str__(self):
+        return str(self.pk) + " | " + str(self.device.ip_address) + ":" + str(self.number)
 
